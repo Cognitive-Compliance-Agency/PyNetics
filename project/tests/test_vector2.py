76ec0aa0c874d9ccd88f1cccd0c1acb8e40e3042
Sterban718 <bhauve2012@gmail.com>
2026-09-06 16:52:05 -0400

    the test works now

--- project/tests/test_vector2.py
+++ project/tests/test_vector2.py
@@ -6,10 +6,12 @@ from pynetics.math import Vector2
 class Vector2Tests(unittest.TestCase):
 
     def test_stores_x_and_y(self):
-        vector = Vector2 (x=2, y=3)
+        x = 2
+        y = 3
+        vector = Vector2 (x, y)
 
-        self.assertEqual(vector.x, 2)
-        self.assertEqual(vector.y, 3)
+        self.assertEqual(vector.x, x)
+        self.assertEqual(vector.y, y)
 
 if __name__ == "__main__":
     unittest.main()
